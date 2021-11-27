fd0b121435bea2ba4f520e6ca9ff87b269c83d57
Renzo <renzo607@gmail.com>
2021-11-27 00:19:15 -0500

    Initial Commit

---
+++ Little Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Little Contacts
+//
+//  Created by Renzo Paul Chamorro on 27/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
